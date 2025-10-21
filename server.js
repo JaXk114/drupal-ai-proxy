d34c2e9b2bcf77b3a675cef99d40b76feaee638e
JaXk114 <jack114b@gmail.com>
2025-10-21 18:11:10 +0100

    fix json2

--- server.js
+++ server.js
@@ -49,4 +49,5 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(10000, () => console.log("Proxy running on port 10000"));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Proxy running on port ${PORT}`));
